ea89651f2fd45235ffc0f3e322866fe3d96a49fa
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-24 00:26:44 +0800

    Instruction

---
+++ exponents.py
@@ -0,0 +1,16 @@
+# # Exercise 15: Write a function called exponent(base, exp) 
+# that returns an int value of base raises to the power of exp.
+
+# Expected output
+
+# Case 1:
+# base = 2
+# exponent = 5
+# 2 raises to the power of 5: 32 
+# i.e. (2 *2 * 2 *2 *2 = 32)
+
+# Case 2:
+# base = 5
+# exponent = 4
+# 5 raises to the power of 4 is: 625 
+# i.e. (5 *5 * 5 *5 = 625)
